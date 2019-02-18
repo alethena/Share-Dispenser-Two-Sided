f913259a32fad7a1438c7e89cbd003e7a105eaae
Benjamin Rickenbacher <b.rickenbacher@intergga.ch>
2019-02-18 16:38:08 +0100

    Extended Documentation

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ There is a breaking change in the npm module `truffle-contract` from version 4.x
 
 The share dispenser allows a company to provide a certain degree of liquidity to their shares. The company 
 
+**State Variables**
 
 The contract uses the following variables:
 
@@ -42,3 +43,43 @@ The buy and sell sides can be separately enabled/disabled through the variables 
 * The XCHF or ALEQ balance held by the dispenser contract can be retrieved through `getERC20Balance` which takes the corresponding contract address as an argument. 
 * To check that a share purchase can happen, the user needs to hold a sufficient amount of XCHF (or ALEQ) but they also need to give a sufficient allowance to the dispenser contract. This is checked using the `getERC20Available` function which takes two arguments, the contract address (ALEQ or XCHF) as well as the potential buyer/seller.
 * In order for the company to retrieve XCHF or ALEQ from the dispenser contract the `retrieveERC20` function is implemented. It expects three arguments, the contract address (ALEQ or XCHF), the address to send the tokens to as well as the amount. This function can only be called by the contract owner.
+
+
+**A note on decimals**
+XCHF has 18 decimals, ALEQ has zero decimals.
+Share prices are entered by the user in Rappen (0.01 XCHF). Hence we need a factor of 10**16 inbetween.
+
+**The buy process**
+
+To buy shares the user first grants a sufficient XCHF allowance to the dispenser smart contract.
+Then the user calls the function `buyShares` with one argument, the number of shares to buy.
+
+1. It is checked that buying is enabled
+2. The number of shares available is fetched and the totalPrice is computed using `getCumulatedPrice`.
+3. It is checked that the dispenser has enough shares and the buyer has sufficient XCHF balance and allowance set.
+4. The usage fee is computed and subtracted from the total price.
+5. Usage fee is transferred to the corresponding address
+6. The remaining XCHF is transferred to the dispenser contract.
+7. The shares are transferred to the buyer.
+8. An event is emitted and the function returns `true`.
+
+Any user can call `buyShares`.
+
+**The sell process**
+
+To buy shares the user first grants a sufficient ALEQ allowance to the dispenser smart contract.
+Then the user calls the function `sellShares` with one argument, the number of shares to sell.
+
+1. It is checked that selling is enabled
+2. The number of XCHF available is fetched and the totalPrice is computed using `getCumulatedBuyBackPrice`.
+3. It is checked that the dispenser has enough XCHF and the seller has sufficient ALEQ balance and allowance set.
+4. The usage fee is computed and subtracted from the total price.
+5. The shares are transferred from the seller to the dispenser contract.
+6. Usage fee is transferred to the corresponding address
+7. The remaining XCHF is transferred to the dispenser seller.
+8. An event is emitted and the function returns `true`.
+
+Any user can call `sellShares`.
+
+
+**Permissions**
--- Solidity/contracts/ShareDispenser.sol
+++ Solidity/contracts/ShareDispenser.sol
@@ -4,6 +4,9 @@ import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
+
+// Overall comment: What does contract do and dependencies...
+
 interface ERC20 {
     function totalSupply() external view returns (uint256);
     function transfer(address to, uint tokens) external returns (bool success);
@@ -14,16 +17,23 @@ interface ERC20 {
 }
 
 contract ShareDispenser is Ownable, Pausable {
-
-    constructor(address initialXCHFContractAddress, address initialALEQContractAddress, address initialusageFeeAddress) public {
+    constructor(
+        address initialXCHFContractAddress, 
+        address initialALEQContractAddress, 
+        address initialusageFeeAddress
+        ) public {
         XCHFContractAddress = initialXCHFContractAddress;
         ALEQContractAddress = initialALEQContractAddress;
         usageFeeAddress = initialusageFeeAddress;
     }
 
+    function () external payable {
+        revert("This contract does not accept Ether."); 
+    }   
+
     using SafeMath for uint256;
 
-    // Relevant Addresses
+    // Variables
 
     address public XCHFContractAddress;     // Address where XCHF is deployed
     address public ALEQContractAddress;     // Address where ALEQ is deployed
@@ -40,7 +50,97 @@ contract ShareDispenser is Ownable, Pausable {
     uint256 public initialNumberOfShares = 10000;
     bool public buyEnabled = true;
     bool public sellEnabled = true;
+
+    // Events 
+
+    event XCHFContractAddressSet(address newXCHFContractAddress);
+    event ALEQContractAddressSet(address newALEQContractAddress);
+    event UsageFeeAddressSet(address newUsageFeeAddress);
+
+    event SharesPurchased(address indexed buyer, uint256 amount);
+    event SharesSold(address indexed seller, uint256 amount);
     
+    event TokensRetrieved(address contractAddress, address to, uint256 amount);
+
+    event UsageFeeSet(uint256 usageFee);
+    event SpreadSet(uint256 spread);
+    event MinPriceSet(uint256 minPrice);
+    event MaxPriceSet(uint256 maxPrice);
+    event InitialNumberOfSharesSet(uint256 initialNumberOfShares);
+
+    event BuyStatusChanged(bool newStatus);
+    event SellStatusChanged(bool newStatus);
+    
+
+    // Function for buying shares
+
+    function buyShares(uint256 numberOfSharesToBuy) public whenNotPaused() returns (bool) {
+        // Check that buying is enabled
+        require(buyEnabled, "Buying is currenty disabled");
+
+        // Fetch the total price
+        address buyer = msg.sender;
+        uint256 sharesAvailable = getERC20Balance(ALEQContractAddress);
+        uint256 totalPrice = getCumulatedPrice(numberOfSharesToBuy, sharesAvailable);
+
+        // Check everything necessary
+        require(sharesAvailable >= numberOfSharesToBuy, "Not enough shares available");
+        require(getERC20Available(XCHFContractAddress, buyer) >= totalPrice, "Payment not authorized or funds insufficient");
+
+        // Compute usage fee and final payment amount
+        uint256 usageFee = totalPrice.mul(usageFeeBSP).div(10000);
+        uint256 paymentAmount = totalPrice.sub(usageFee);
+
+        // Instantiate contracts
+        ERC20 ALEQ = ERC20(ALEQContractAddress);
+        ERC20 XCHF = ERC20(XCHFContractAddress);
+
+        // Transfer usage fee and payment amount
+        require(XCHF.transferFrom(buyer, usageFeeAddress, usageFee), "Usage fee transfer failed");
+        require(XCHF.transferFrom(buyer, address(this), paymentAmount), "XCHF payment failed");
+
+        // Transfer the shares
+        require(ALEQ.transfer(buyer, numberOfSharesToBuy), "Share transfer failed");
+
+        emit SharesPurchased(buyer, numberOfSharesToBuy);
+        return true;
+    }
+
+    // Function for selling shares
+
+    function sellShares(uint256 numberOfSharesToSell) public whenNotPaused() returns (bool) {
+        // Check that selling is enabled
+        require(sellEnabled, "Selling is currenty disabled");
+
+        // Fetch buyback price
+        address seller = msg.sender;
+        uint256 XCHFAvailable = getERC20Balance(XCHFContractAddress);
+        uint256 sharesAvailable = getERC20Balance(ALEQContractAddress);
+
+        uint256 buyBackPrice = getCumulatedBuyBackPrice(numberOfSharesToSell, sharesAvailable);
+
+        // Checks
+        require(XCHFAvailable >= buyBackPrice, "Reserves to small to buy back this amount of shares");
+        require(getERC20Available(ALEQContractAddress, seller) >= numberOfSharesToSell, "Seller doesn't have enough shares");
+
+        // Compute usage fee and final payment amount
+        uint256 usageFee = buyBackPrice.mul(usageFeeBSP).div(10000);
+        uint256 paymentAmount = buyBackPrice.sub(usageFee);
+
+        // Instantiate contracts
+        ERC20 ALEQ = ERC20(ALEQContractAddress);
+        ERC20 XCHF = ERC20(XCHFContractAddress);
+
+        // Transfer the shares
+        require(ALEQ.transferFrom(seller, address(this), numberOfSharesToSell), "Share transfer failed");
+
+        // Transfer usage fee and payment amount
+        require(XCHF.transfer(usageFeeAddress, usageFee), "Usage fee transfer failed");
+        require(XCHF.transfer(seller, paymentAmount), "XCHF payment failed");
+
+        emit SharesSold(seller, numberOfSharesToSell);
+        return true;
+    }
 
     // Getters for ERC20 balances
 
@@ -84,79 +184,13 @@ contract ShareDispenser is Ownable, Pausable {
         return getCumulatedPrice(amount, supply+amount).mul(spreadBSP).div(10000); // For symmetry reasons
     }
 
-    // Function for buying shares
-
-    function buyShares(uint256 numberOfSharesToBuy) public whenNotPaused() returns (bool) {
-        require(buyEnabled, "Buying is currenty disabled");
-        // Fetch the total price
-        address buyer = msg.sender;
-        uint256 sharesAvailable = getERC20Balance(ALEQContractAddress);
-        uint256 totalPrice = getCumulatedPrice(numberOfSharesToBuy, sharesAvailable);
-
-        // Check everything necessary
-        require(sharesAvailable >= numberOfSharesToBuy, "Not enough shares available");
-        require(getERC20Available(XCHFContractAddress, buyer) >= totalPrice, "Payment not authorized or funds insufficient");
-
-        // Compute usage fee and final payment amount
-        uint256 usageFee = totalPrice.mul(usageFeeBSP).div(10000);
-        uint256 paymentAmount = totalPrice.sub(usageFee);
-
-        // Instantiate contracts
-        ERC20 ALEQ = ERC20(ALEQContractAddress);
-        ERC20 XCHF = ERC20(XCHFContractAddress);
-
-        // Transfer usage fee and payment amount
-        require(XCHF.transferFrom(buyer, usageFeeAddress, usageFee), "Usage fee transfer failed");
-        require(XCHF.transferFrom(buyer, address(this), paymentAmount), "XCHF payment failed");
-
-        // Transfer the shares
-        require(ALEQ.transfer(buyer, numberOfSharesToBuy), "Share transfer failed");
-
-        emit SharesPurchased(buyer, numberOfSharesToBuy);
-        return true;
-    }
-
-    // Function for selling shares
-
-    function sellShares(uint256 numberOfSharesToSell) public whenNotPaused() returns (bool) {
-        require(sellEnabled, "Selling is currenty disabled");
-
-        address seller = msg.sender;
-        uint256 XCHFAvailable = getERC20Balance(XCHFContractAddress);
-        uint256 sharesAvailable = getERC20Balance(ALEQContractAddress);
-
-        uint256 buyBackPrice = getCumulatedBuyBackPrice(numberOfSharesToSell, sharesAvailable);
-
-        // Checks
-        require(XCHFAvailable >= buyBackPrice, "Reserves to small to buy back this amount of shares");
-        require(getERC20Available(ALEQContractAddress, seller) >= numberOfSharesToSell, "Seller doesn't have enough shares");
-
-        // Compute usage fee and final payment amount
-        uint256 usageFee = buyBackPrice.mul(usageFeeBSP).div(10000);
-        uint256 paymentAmount = buyBackPrice.sub(usageFee);
-
-        // Instantiate contracts
-        ERC20 ALEQ = ERC20(ALEQContractAddress);
-        ERC20 XCHF = ERC20(XCHFContractAddress);
-
-        // Transfer the shares
-        require(ALEQ.transferFrom(seller, address(this), numberOfSharesToSell), "Share transfer failed");
-
-        // Transfer usage fee and payment amount
-        require(XCHF.transfer(usageFeeAddress, usageFee), "Usage fee transfer failed");
-        require(XCHF.transfer(seller, paymentAmount), "XCHF payment failed");
-
-        emit SharesSold(seller, numberOfSharesToSell);
-        return true;
-    }
-
+    
     function retrieveERC20(address contractAddress, address to, uint256 amount) public onlyOwner() returns(bool) {
         ERC20 contractInstance = ERC20(contractAddress);
         require(contractInstance.transfer(to, amount), "Transfer failed");
         emit TokensRetrieved(contractAddress, to, amount);
         return true;
     }
-
 
     // Setters for addresses
 
@@ -224,25 +258,7 @@ contract ShareDispenser is Ownable, Pausable {
         emit SellStatusChanged(newStatus);
     }
 
-    // Events 
 
-    event XCHFContractAddressSet(address newXCHFContractAddress);
-    event ALEQContractAddressSet(address newALEQContractAddress);
-    event UsageFeeAddressSet(address newUsageFeeAddress);
-
-    event SharesPurchased(address indexed buyer, uint256 amount);
-    event SharesSold(address indexed seller, uint256 amount);
-    
-    event TokensRetrieved(address contractAddress, address to, uint256 amount);
-
-    event UsageFeeSet(uint256 usageFee);
-    event SpreadSet(uint256 spread);
-    event MinPriceSet(uint256 minPrice);
-    event MaxPriceSet(uint256 maxPrice);
-    event InitialNumberOfSharesSet(uint256 initialNumberOfShares);
-
-    event BuyStatusChanged(bool newStatus);
-    event SellStatusChanged(bool newStatus);
 
     // Helper functions
 

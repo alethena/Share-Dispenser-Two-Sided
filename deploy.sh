6c69f06208d21f08827f70cb514aef28e3830334
Benjamin Rickenbacher <b.rickenbacher@intergga.ch>
2019-03-05 12:44:54 +0100

    Save before polyfills

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# My first script
+
+cd ng 
+npm install
+ng build --prod --aot
+/Users/benjaminrickenbacher/.anaconda/navigator/a.tool
+aws s3 rm s3://dispenser.alethena.com --recursive
+aws s3 cp ./dist/ s3://dispenser.alethena.com --recursive
